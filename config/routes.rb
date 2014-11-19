0b29b7816e2c9bbaa1847e31ded0bf0b76e68b2c
ErikaLim <Erika.megan.lim@gmail.com>
2014-11-19 10:24:01 -0800

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  root to: "pages#index"
+  root to: "pages#index", as: :index
+
+  get 'verse_2' => "pages#verse_2"
+
+  get 'verse_3' => "pages#verse_3"
+
+  get 'verse_4' => "pages#verse_4"
+
+  get 'index' => "pages#index"
+
 end
